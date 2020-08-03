452bd70428af9d8eb529fc97f542edf1fdc4c5b8
werowe <werowe@walkerrowe.com>
2020-08-03 10:06:16 +0300

    Add files via upload

--- ratings.py
+++ ratings.py
@@ -1,51 +1,55 @@
 import simplejson as json
 import os
-
-fileStr='/Users/walkerrowe/Documents/imdb/100.basics.tsv'
-f = open(fileStr, 'r')
-o = open('/Users/walkerrowe/Documents/imdb/100.basics.json', 'w')
+import sys
 
 
 def formatJSON(l, cols):
     json = {}
 
     for i in range(len(l)):
-        print(l[i], cols[i])
         json[cols[i]] = {"S": l[i]}
 
     return json
 
+def process(fileStr):
 
-cols = []
+    f = open('/Users/walkerrowe/Documents/imdb/'+ fileStr, 'r')
+    o = open('/Users/walkerrowe/Documents/imdb/' + fileStr + '.json', 'w')
 
-o.write('{ "title": [' + '\n')
+    print('processing ' + fileStr)
 
-i = 0
-filePos=0
-f.seek(0, os.SEEK_END)
-f.seek(0)
+    cols = []
 
-for l in f:
-    ln = l.strip("\\N").strip("\n")
-    filePos = filePos + len(l)
-    f.seek(filePos)
-    line = ln.split('\t')
-    if i == 0:
-        i = 1
-        cols = line
-        print("cols=", cols)
-    else:
-        if filePos < os.stat(fileStr).st_size:
-            o.write ('{ "PutRequest": {"Item":')
-            str = json.dumps(formatJSON(line, cols)) + '}},\n'
-            o.write(json.dumps(formatJSON(line, cols)) + '}},\n')
+    o.write('{ "title": [' + '\n')
+
+    i = 0
+    filePos=0
+    f.seek(0, os.SEEK_END)
+    f.seek(0)
+
+    for l in f:
+        ln = l.strip("\\N").strip("\n")
+        filePos = filePos + len(l)
+        f.seek(filePos)
+        line = ln.split('\t')
+        if i == 0:
+            i = 1
+            cols = line
         else:
-            o.write('{ "PutRequest": {"Item":')
-            str = json.dumps(formatJSON(line, cols)) + '}}'
-            o.write(str)
+            if filePos < os.stat(fileStr).st_size:
+                o.write ('{ "PutRequest": {"Item":')
+                str = json.dumps(formatJSON(line, cols)) + '}},\n'
+                o.write(json.dumps(formatJSON(line, cols)) + '}},\n')
+            else:
+                o.write('{ "PutRequest": {"Item":')
+                str = json.dumps(formatJSON(line, cols)) + '}}'
+                o.write(str)
+
+    o.write(']}')
+    o.close()
 
 
 
-o.write(']}')
-o.close()
+if __name__ == "__main__" :
 
+    process(sys.argv[1])
